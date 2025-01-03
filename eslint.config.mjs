25950a8353b752c82e6bf3bdfd0d6deb4f96098d
Tanguy Verdoodt <tanguy.verdoodt@gmail.com>
2025-01-03 20:16:49 +0100

    eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import eslintPlugin from "@typescript-eslint/eslint-plugin";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -9,6 +10,28 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals"),
+  {
+    files: ["**/*.{js,jsx,ts,tsx}"], // Inclure ces fichiers
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: "module",
+      parser: "@typescript-eslint/parser",
+    },
+    plugins: {
+      "@typescript-eslint": eslintPlugin,
+    },
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@next/next/no-html-link-for-pages": "off",
+    },
+  },
+  {
+    ignores: ["node_modules/**", ".next/**", "dist/**"], // Ignorer ces dossiers
+  },
+];
 
 export default eslintConfig;
